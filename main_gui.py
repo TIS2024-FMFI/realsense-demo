12b552147e09b468e3be1db2df04cf93c6f9c987
Norbert Krivy <norokrivy@gmail.com>
2025-01-19 17:31:59 +0100

    Final core of code for measure distances and button visibility updates.

--- main_gui.py
+++ main_gui.py
@@ -1,15 +1,10 @@
-# ----------------------------------------------------------------------------
-# -                        Open3D: www.open3d.org                            -
-# ----------------------------------------------------------------------------
-# Copyright (c) 2018-2023 www.open3d.org
-# SPDX-License-Identifier: MIT
-# ---------------------------------------------------------------------------
 import numpy as np
 import open3d as o3d
 from open3d.visualization import gui, rendering
-from sensors.realsense_recorder import scan
-from src.run_system import get_pointcloud
 
+APP_NAME = "Realsense APP (FMFI UK project)"
+
+SHADER_STYLE = "defaultUnlit"
 
 BUTTON_START_STREAM_ID = "start_stream"
 BUTTON_STOP_STREAM_ID = "stop_stream"
@@ -19,7 +14,6 @@ BUTTON_START_MEASURE_ID = "start_measure"
 BUTTON_STOP_MEASURE_ID = "stop_measure"
 BUTTON_SHOW_SCAN_ID = "show_scan"
 BUTTON_HIDE_SCAN_ID = "hide_scan"
-BUTTON_CALIBRATE_ID = "calibrate"
 
 MAIN_SCREEN_ID = "main_screen"
 
@@ -34,18 +28,32 @@ class AppWindow:
         BUTTON_STOP_MEASURE_ID: "STOP MEASURING",
         BUTTON_SHOW_SCAN_ID: "SHOW SCAN",
         BUTTON_HIDE_SCAN_ID: "HIDE SCAN",
-        BUTTON_CALIBRATE_ID: "CALIBRATE",
     }
+
+    # FOR MEASUREMENT IN PLY
+    _picked_indicates = []
+    _picked_positions = []
+    _picked_num = 0
 
     def __init__(self, width, height):
         self.pcd = None
+        self.pcd_kdtree = None
+
         self.buttons = dict()
         self.button_is_clicked_mapper = dict()
 
         gui.Application.instance.initialize()
         self._set_scene(width, height)
 
-    def create_button_to_bar(self, name, function, visibility=True):
+    def _on_layout(self, layout_context):
+        r = self.window.content_rect
+        self._scene.frame = r
+        width = 17 * layout_context.theme.font_size
+        height = self.height
+        self._left_panel.frame = gui.Rect(r.get_left(), r.y, width,
+                                          height)
+
+    def create_button_to_bar(self, name: str, function, visibility=True):
         button = gui.Button(name)
 
         button.horizontal_padding_em = 0.5
@@ -65,22 +73,10 @@ class AppWindow:
         self.window.set_on_layout(self._on_layout)
         self.window.add_child(self._left_panel)
 
-    def _set_scene(self, width, height):
-        self.window = gui.Application.instance.create_window(
-            "Realsense APP (FMFI UK project)", width, height)
-        w = self.window
-        self.width = width
-        self.height = height
+    def update_distance(self, distance: float):
+        self.distance_text_label.text = f"Distance {str(round(distance, 2))} m"
 
-        # 3D widget
-        self._scene = gui.SceneWidget()
-        self._scene.scene = rendering.Open3DScene(w.renderer)
-
-        em = w.theme.font_size
-
-        self._left_panel = gui.Vert(
-            0, gui.Margins(0.25 * em, 0.25 * em, 0.25 * em, 0.25 * em))
-
+    def buttons_to_bar(self):
         self.create_button_to_bar(BUTTON_START_STREAM_ID, function=self.start_stream)
         self.create_button_to_bar(BUTTON_STOP_STREAM_ID, function=self.stop_stream, visibility=False)
 
@@ -93,28 +89,160 @@ class AppWindow:
         self.create_button_to_bar(BUTTON_STOP_MEASURE_ID, function=self.stop_measure, visibility=False)
 
         self.create_button_to_bar(BUTTON_EXPORT_ID, function=self.export_file)
-        self.create_button_to_bar(BUTTON_CALIBRATE_ID, function=self.calibrate_camera)
+
+    def _set_scene(self, width, height):
+        self.window = gui.Application.instance.create_window(APP_NAME, width, height)
+        w = self.window
+        self.width = width
+        self.height = height
+
+        # 3D widget
+        self._scene = gui.SceneWidget()
+        self._scene.scene = rendering.Open3DScene(w.renderer)
+
+        em = w.theme.font_size
+        size = 0.25 * em
+        self._left_panel = gui.Vert(0, gui.Margins(size, size, size, size))
+
+        self.buttons_to_bar()
+
+        self.distance_text_label = self.create_distance_label()
+        self._left_panel.add_child(self.distance_text_label)
 
         w.set_on_layout(self._on_layout)
         w.add_child(self._scene)
         w.add_child(self._left_panel)
 
-    def start_stream(self):
+    def _start_measure_event(self, event):
+        # CTRL + Left click
+        if (event.type == gui.MouseEvent.Type.BUTTON_DOWN and
+                event.is_button_down(gui.MouseButton.LEFT) and
+                event.is_modifier_down(gui.KeyModifier.CTRL)):
+
+            def calculate_distance_depth_image(depth_image):
+                x = event.x - self._scene.frame.x
+                y = event.y - self._scene.frame.y
+                depth = np.asarray(depth_image)[y, x]
+                self.process_points(x, y, depth)
+
+            self._scene.scene.scene.render_to_depth_image(calculate_distance_depth_image)
+            return gui.Widget.EventCallbackResult.HANDLED
+
+        # CTRL + Right click
+        elif (event.type == gui.MouseEvent.Type.BUTTON_DOWN and
+              event.is_button_down(gui.MouseButton.RIGHT) and
+              event.is_modifier_down(gui.KeyModifier.CTRL)):
+
+            self.remove_pick()
+            return gui.Widget.EventCallbackResult.HANDLED
+
+        return gui.Widget.EventCallbackResult.IGNORED
+
+    def _calc_prefer_indicate(self, point):
+        [k, idx, _] = self.pcd_kdtree.search_knn_vector_3d(point, 1)
+        return idx[0]
+
+    @staticmethod
+    def calculate_distance(point1: list[float], point2: list[float]) -> float:
+        point1 = np.array(point1)
+        point2 = np.array(point2)
+        return np.linalg.norm(point1 - point2)
+
+    @staticmethod
+    def create_distance_label() -> gui.Label:
+        label = gui.Label("Distance: -- m")
+        label.text_color = gui.Color(255.0, 255.0, 255.0)
+        label.visible = False
+        return label
+
+    @staticmethod
+    def run():
+        gui.Application.instance.run()
+
+    def update_ply_scan_view(self, button_id):
+        button_clicked = self.button_is_clicked_mapper[button_id]
+
+        geometry_name = "pcd"
+        scene = self._scene.scene
+        pcd_path = "dataset/realsense/scene/integrated.ply"  # todo get self.ply_file from streaming and stitching the file
+        pcd = o3d.io.read_point_cloud(pcd_path)
+
+        material = rendering.MaterialRecord()
+        material.point_size = 5.0
+
+        if button_clicked:
+            scene.add_geometry(geometry_name, pcd, material)
+        else:
+            scene.remove_geometry(geometry_name)
+
+    def change_button_text(self, button_id):
+        button = self.buttons[button_id]
+        is_clicked = self.button_is_clicked_mapper[button_id]
+        button_text = self.button_stop_text[button_id] if is_clicked else self.button_start_text[button_id]
+        button.text = button_text
+
+    def remove_pick(self):
+        if self._picked_num > 0:
+            idx = self._picked_indicates.pop()
+            point = self._picked_positions.pop()
+
+            print(
+                f"Undo pick: #{idx} at ({point[0]}, {point[1]}, {point[2]})")
+
+            self._scene.scene.remove_geometry(
+                'sphere' + str(self._picked_num))
+            self._picked_num -= 1
+            self._scene.force_redraw()
+        else:
+            print('Undo nothing!')
+
+    def process_points(self, x, y, z):
+        NULL_DEPTH = 1.0
+        if z == NULL_DEPTH:
+            return
+        world_coord = self._scene.scene.camera.unproject(
+            x, y, z, self._scene.frame.width, self._scene.frame.height)
+        idx = self._calc_prefer_indicate(world_coord)
+        picked_point = np.asarray(self.pcd.points)[idx]
+
+        self._picked_num += 1
+        self._picked_indicates.append(idx)
+        self._picked_positions.append(picked_point)
+
+        if self._picked_num % 2 == 0:
+            distance = self.calculate_distance(self._picked_positions[-1], self._picked_positions[-2])
+            self.update_distance(distance)
+
+    # BUTTON FUNCTIONS
+    def start_measure(self):
+        if not self.pcd:
+            return
         visibility_after_click_mapper = {
             BUTTON_START_STREAM_ID: False,
-            BUTTON_STOP_STREAM_ID: True,
-            BUTTON_START_SCAN_ID: True,
+            BUTTON_STOP_STREAM_ID: False,
+            BUTTON_START_SCAN_ID: False,
             BUTTON_EXPORT_ID: False,
             BUTTON_START_MEASURE_ID: False,
-            BUTTON_STOP_MEASURE_ID: False,
+            BUTTON_STOP_MEASURE_ID: True,
             BUTTON_SHOW_SCAN_ID: False,
             BUTTON_HIDE_SCAN_ID: False,
-            BUTTON_CALIBRATE_ID: False,
         }
         self.update_visiblity(visibility_after_click_mapper)
-        pass  # todo
 
-    def stop_stream(self):
+        self.distance_text_label.visible = True
+
+        self.pcd = o3d.io.read_point_cloud("ply_files_test/skuska_akvarko.ply")
+        self.pcd_kdtree = o3d.geometry.KDTreeFlann(self.pcd)
+
+        self._scene.set_on_mouse(self._start_measure_event)  # todo when clicked on start measuring
+
+        material = rendering.MaterialRecord()
+        material.shader = SHADER_STYLE
+        material.point_size = 5.0
+
+        self._scene.scene.add_geometry(BUTTON_START_MEASURE_ID, self.pcd, material)
+
+    def stop_measure(self):
         visibility_after_click_mapper = {
             BUTTON_START_STREAM_ID: True,
             BUTTON_STOP_STREAM_ID: False,
@@ -124,23 +252,17 @@ class AppWindow:
             BUTTON_STOP_MEASURE_ID: False,
             BUTTON_SHOW_SCAN_ID: True,
             BUTTON_HIDE_SCAN_ID: False,
-            BUTTON_CALIBRATE_ID: True,
         }
         self.update_visiblity(visibility_after_click_mapper)
-        pass  # todo
 
-    def start_scan(self):
-        gui.Application.instance.post_to_main_thread(self.window, scan)
-        
-        #gui.Application.instance.post_to_main_thread(
-        #            self.window, get_pointcloud)
+        self.distance_text_label.visible = False
+        self.distance_text_label.text = f"Distance -- m"
 
-        self.pcd = get_pointcloud()  #todo ak get_pointcloud vracia pcd, tak by som to dal do self.pcd a pracoval neskor s tymito datami
-
+        self._scene.set_on_mouse(None)
+        self._scene.scene.remove_geometry(BUTTON_START_MEASURE_ID)
 
     def show_ply_scene(self):
         if not self.pcd:
-            print("No PLY file defined.")
             return
 
         visibility_after_click_mapper = {
@@ -152,14 +274,13 @@ class AppWindow:
             BUTTON_STOP_MEASURE_ID: False,
             BUTTON_SHOW_SCAN_ID: False,
             BUTTON_HIDE_SCAN_ID: True,
-            BUTTON_CALIBRATE_ID: False,
         }
         self.update_visiblity(visibility_after_click_mapper)
 
         scene = self._scene.scene
 
         material = rendering.MaterialRecord()
-        material.shader = "defaultUnlit"
+        material.shader = SHADER_STYLE
         material.point_size = 5.0
 
         scene.add_geometry(MAIN_SCREEN_ID, self.pcd, material)
@@ -174,78 +295,70 @@ class AppWindow:
             BUTTON_STOP_MEASURE_ID: False,
             BUTTON_SHOW_SCAN_ID: True,
             BUTTON_HIDE_SCAN_ID: False,
-            BUTTON_CALIBRATE_ID: True,
         }
         self.update_visiblity(visibility_after_click_mapper)
+
+        self.distance_text_label.visible = False
 
         scene = self._scene.scene
         scene.remove_geometry(MAIN_SCREEN_ID)
 
-    def export_file(self):
+    def start_scan(self):
+        from sensors.realsense_recorder import scan
+        from src.run_system import get_pointcloud
+
+        gui.Application.instance.post_to_main_thread(self.window, scan)
+
+        # gui.Application.instance.post_to_main_thread(
+        #            self.window, get_pointcloud)
+
+        # get_pointcloud()
+
         pass  # todo
 
-    def start_measure(self):
-        if not self.pcd:
-            print("No PLY file defined.")
-            return
-
+    def start_stream(self):
         visibility_after_click_mapper = {
+            BUTTON_START_STREAM_ID: False,
+            BUTTON_STOP_STREAM_ID: True,
+            BUTTON_START_SCAN_ID: True,
+            BUTTON_EXPORT_ID: False,
             BUTTON_START_MEASURE_ID: False,
-            BUTTON_STOP_MEASURE_ID: True,
+            BUTTON_STOP_MEASURE_ID: False,
+            BUTTON_SHOW_SCAN_ID: False,
+            BUTTON_HIDE_SCAN_ID: False,
         }
         self.update_visiblity(visibility_after_click_mapper)
 
+        pass  # todo
 
-        def pick_points(pcd):
-            print("1) Shift + Left Click to select points.")
-            print("2) Press 'Q' to finish and close the window.")
-
-            vis = o3d.visualization.VisualizerWithEditing()
-            vis.create_window()
-            vis.add_geometry(pcd)
-            vis.run()
-            vis.close()
-
-            return vis.get_picked_points()
-
-        picked_indices = pick_points(self.pcd)
-
-        if len(picked_indices) >= 2:
-            print("Picked points indices:", picked_indices)
-            point1 = np.asarray(self.pcd.points)[picked_indices[0]]
-            point2 = np.asarray(self.pcd.points)[picked_indices[1]]
-
-            # Calculate the Euclidean distance
-            distance = np.linalg.norm(point1 - point2)
-            print(f"Distance between points: {distance}")
-        else:
-            print(f"Expected at least 2 points, but picked {len(picked_indices)}. Please try again.")
-
-        self.run()
-
-    def stop_measure(self):
+    def stop_stream(self):
         visibility_after_click_mapper = {
+            BUTTON_START_STREAM_ID: True,
+            BUTTON_STOP_STREAM_ID: False,
+            BUTTON_START_SCAN_ID: True,
+            BUTTON_EXPORT_ID: True,
             BUTTON_START_MEASURE_ID: True,
             BUTTON_STOP_MEASURE_ID: False,
+            BUTTON_SHOW_SCAN_ID: True,
+            BUTTON_HIDE_SCAN_ID: False,
+        }
+        self.update_visiblity(visibility_after_click_mapper)
+        pass  #
+
+    def export_file(self):
+        visibility_after_click_mapper = {
+            BUTTON_START_STREAM_ID: True,
+            BUTTON_STOP_STREAM_ID: False,
+            BUTTON_START_SCAN_ID: True,
+            BUTTON_EXPORT_ID: True,
+            BUTTON_START_MEASURE_ID: True,
+            BUTTON_STOP_MEASURE_ID: False,
+            BUTTON_SHOW_SCAN_ID: True,
+            BUTTON_HIDE_SCAN_ID: False,
         }
         self.update_visiblity(visibility_after_click_mapper)
 
         pass  # todo
-
-    def calibrate_camera(self):
-        pass  #todo kalibracia asi nebude potrebna
-
-    def _on_layout(self, layout_context):
-        r = self.window.content_rect
-        self._scene.frame = r
-        width = 17 * layout_context.theme.font_size
-        height = self.height
-        self._left_panel.frame = gui.Rect(r.get_left(), r.y, width,
-                                          height)
-
-    @staticmethod
-    def run():
-        gui.Application.instance.run()
 
 
 def main():
